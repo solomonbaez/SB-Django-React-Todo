2a8009be7e98026e59ecbaec10e92c333bd26155
solomonbaez <solomon.james.baez@gmail.com>
2023-08-03 15:37:00 -0700

    Sketched out naive function-based views

--- base/urls.py
+++ base/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.healthCheck, name="health-check"),
+    path("health-check/", views.healthCheck, name="health-check"),
+    path("", views.homeView, name="home"),
+    path("user-login/", views.loginView, name="user-login"),
+    path("user-logout/", views.logoutView, name="user-logout"),
 ]
--- base/views.py
+++ base/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def healthCheck(request):
     return render(request, "base/health-check.html")
+
+
+def homeView(request):
+    return render(request, "base/home.html")
+
+
+def loginView(request):
+    return render(request, "base/home.html")
+
+
+def logoutView(request):
+    return render(request, "base/home.html")
