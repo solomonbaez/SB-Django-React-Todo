025f77410ef2c32f2b1b3f20da2459145bdb6c28
solomonbaez <solomon.james.baez@gmail.com>
2023-08-03 15:51:49 -0700

    Added basic Note model to base/models.py

--- base/models.py
+++ base/models.py
@@ -1,1 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Note(models.Model):
+    # link to poster, cascade deletion
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, null=True)
+    description = models.TextField(max_length=200, null=True, blank=True)
+
+    updated = models.DateField(auto_now=True)
+    created = models.DateField(auto_now_add=True)
+
+    # specify metadata
+    class Meta:
+        ordering = ["-updated", "-created"]
+
+    # reference
+    def __str__(self):
+        return self.title
