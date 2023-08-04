c38e3dbf4bbbf092258c0d310de762f2c835ae0a
solomonbaez <solomon.james.baez@gmail.com>
2023-08-03 22:55:46 -0700

    Added naive user-login

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Note
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .models import Note, User
 
 
 def healthCheck(request):
@@ -13,7 +14,20 @@ def homeView(request):
 
 
 def loginView(request):
-    return render(request, "base/home.html")
+    if request.method == "POST":
+        username = request.POST.get("username").lower()
+        password = request.POST.get("password")
+
+        user = User.objects.get(username=username)
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect("home")
+
+        else:
+            print("Failed to authenticate")
+
+    return render(request, "base/user-login.html")
 
 
 def logoutView(request):
