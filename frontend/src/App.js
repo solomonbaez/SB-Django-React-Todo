022e1fd840e28acd5376f434755e428b054a46d5
solomonbaez <solomon.james.baez@gmail.com>
2023-08-07 16:18:07 -0700

    Added App routing with a sample note page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import './App.css';
-import Header from "./components/Header"
-import Home from "./pages/Home"
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import NotePage from "./pages/NotePage";
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Home/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/note/:id" element={<NotePage/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/NotePage.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const NotePage = () => {
+    return (
+        <div>
+            <h1>Test Note</h1>
+        </div>
+    );
+};
+
+export default NotePage;
