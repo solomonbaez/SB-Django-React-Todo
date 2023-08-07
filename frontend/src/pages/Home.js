57df1178c06985b1c35f167940a0f52a8fede3fc
solomonbaez <solomon.james.baez@gmail.com>
2023-08-07 13:20:05 -0700

    Replaced django frontedn with React api calls

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,21 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 
 const Home = () => {
+
+    // initialize state
+    let [notes, setNotes] = useState([]);
+
+    useEffect(() => {
+        getNotes()
+    }, [])
+
+    let getNotes = async () => {
+        // fetch requires async await
+        let response = await fetch("http://127.0.0.1:8000/api/notes");
+        let data = response.json();
+        console.log("DATA:", data)
+        setNotes(data)
+    }
     
     return (
         <div className="App">
--- todo/urls.py
+++ todo/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
-    path("", include("base.urls")),
+    path("api/", include("base.urls")),
 ]
