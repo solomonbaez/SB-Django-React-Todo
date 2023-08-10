e632039d2b7f0dbc1ae037d67bdf23ae96c41de1
solomonbaez <solomon.james.baez@gmail.com>
2023-08-10 10:43:38 -0700

    Made the dashboard more informative

--- frontend/src/components/ListItem.js
+++ frontend/src/components/ListItem.js
@@ -1,11 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+let getContent = (note) => {
+  let content = note.description.replaceAll("\n", " ");
+  content = content.replaceAll(note.title, "");
+
+  if (content.length > 50) {
+    return content.slice(0, 50) + "...";
+  } else {
+    return content;
+  }
+};
+
 const ListItem = ({ note }) => {
   return (
     <Link to={`/note/${note.id}`}>
       <div className="notes-list-item">
         <h3>{note.title}</h3>
+        <p>
+          <span>{note.updated}</span>
+          {getContent(note)}
+        </p>
       </div>
     </Link>
   );
