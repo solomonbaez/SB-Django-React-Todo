382832d2e5ad57e229fe8b6160d2775d8d1d8940
solomonbaez <solomon.james.baez@gmail.com>
2023-08-10 10:56:53 -0700

    Integrated React to be hosted by Django

--- base/urls.py
+++ base/urls.py
@@ -9,9 +9,4 @@ urlpatterns = [
     path("create/", views.apiCreateNote, name="create-note"),
     path("notes/<str:pk>/update", views.apiUpdateNote, name="update-note"),
     path("notes/<str:pk>/delete", views.apiDeleteNote, name="delete-note"),
-    # path("", views.homeView, name="home"),
-    # path("user-login", views.loginView, name="user-login"),
-    # path("user-logout", views.logoutView, name="user-logout"),
-    # path("create-note/", views.createNote, name="create-note"),
-    # path("update-note/<str:pk>", views.updateNote, name="update-note"),
 ]
